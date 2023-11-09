59e590341c6c824498fe95f374a8dd6f97a019bb
zdzd3643 <zdzd3643@gmail.com>
2023-11-09 12:32:35 +0900

    FF

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let isrc = 'https://image.cine21.com/resize/cine21/person/2019/1030/11_35_56__5db8f70c422b9[X252,310].jpg'
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={isrc} 
+        style={{borderRadius:'50%'}}
+        className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -15,7 +18,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          공유도 리액트한다~!
         </a>
       </header>
     </div>
